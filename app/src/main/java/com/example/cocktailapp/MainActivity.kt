4e29a4122419ef81db580c2d91857084af741728
Ilyes-Cheikh <cheikh.ilyes10@gmail.com>
2023-11-11 17:35:02 +0100

    adding navigate function to navigate within fragments

--- app/src/main/java/com/example/cocktailapp/MainActivity.kt
+++ app/src/main/java/com/example/cocktailapp/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.example.cocktailapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cocktailapp.ui.categories.CategoriesFragment
+import com.example.cocktailapp.ui.favorites.FavoritesFragment
+import com.example.cocktailapp.ui.ingredients.IngredientsFragment
+import com.example.cocktailapp.ui.search.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: BottomNavigationView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tabLayout = findViewById(R.id.tab_layout)
+        displaySearchFragment()
+        navigate()
     }
+
+    private fun displaySearchFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, SearchFragment.newInstance())
+            .commit()
+    }
+    private fun displayCategoriesFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, CategoriesFragment.newInstance())
+            .commit()
+    }
+    private fun displayIngredientsFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, IngredientsFragment.newInstance())
+            .commit()
+    }
+    private fun displayFavoritesFragment(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container_view, FavoritesFragment.newInstance())
+            .commit()
+    }
+
+    private  fun navigate(){
+        tabLayout.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.search_page -> {
+                    displaySearchFragment()
+                    true
+                }
+                R.id.categories_page -> {
+                    displayCategoriesFragment()
+                    true
+                }
+                R.id.ingredients_page -> {
+                    displayIngredientsFragment()
+                    true
+                }
+                R.id.favorites_page -> {
+                    displayFavoritesFragment()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/cocktailapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/cocktailapp/ui/search/SearchFragment.kt
@@ -19,16 +19,16 @@ private const val ARG_PARAM2 = "param2"
  */
 class SearchFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //private var param1: String? = null
+  //  private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //override fun onCreate(savedInstanceState: Bundle?) {
+       // super.onCreate(savedInstanceState)
+      //  arguments?.let {
+       //     param1 = it.getString(ARG_PARAM1)
+        //    param2 = it.getString(ARG_PARAM2)
+       // }
+   // }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,12 +49,7 @@ class SearchFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            SearchFragment()
     }
 }
